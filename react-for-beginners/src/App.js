231e388c00b22d13839238f0709f01f9739f372b
kim <jayfulkr@gmail.com>
2023-04-18 21:43:24 +0900

    4.18 todo list

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,22 +1,43 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  function Hello() {
-    // Cleanup
-    useEffect(() => {
-      console.log('created :)');
-      return () => console.log('bye :(');
-    }, []);
-    return <h1>Hello</h1>;
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  function onChange(e) {
+    setToDo(e.target.value);
   }
-  function onClick() {
-    setShowing(prev => !prev);
+  function onSubmit(e) {
+    e.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos(prev => [...prev, toDo]);
+    setToDo('');
+    console.log(toDos);
+  }
+  function Todos() {
+    return (
+      <ul>
+        {toDos.map((ele, index) => {
+          return <li key={index}>{ele}</li>;
+        })}
+      </ul>
+    );
   }
   return (
     <>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'hide' : 'show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your to do...'
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <Todos />
     </>
   );
 }
