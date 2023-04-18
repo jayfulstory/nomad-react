4d8967e360c931a12260affdbcf4ca49cd2d253c
kim <jayfulkr@gmail.com>
2023-04-18 15:20:21 +0900

    4.18

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,44 @@
 import Button from './Button.js';
 import styles from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  function onClick() {
+    setCounter(prev => prev + 1);
+  }
+  function onChange(e) {
+    setKeyword(e.target.value);
+  }
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 4) {
+      console.log("i run when 'keyword' changes.");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log('i run when keyword & counter change');
+  }, [keyword, counter]);
+
+  useEffect(() => {
+    console.log('i run only once');
+  }, []);
   return (
     <>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={'Continue'} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search here...'
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+      {/* <h1 className={styles.title}>Welcome back!</h1> */}
+      {/* <Button text={'Continue'} /> */}
     </>
   );
 }
